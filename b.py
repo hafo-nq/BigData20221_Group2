4df70c4d7f202a55f91b5b479935acf6c0f72b56
Nguyen Quoc Hao <hafo.nq@gmail.com>
2022-12-26 11:12:11 +0700

    delete stuffs

--- b.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_parquet('part-00000-75b178b3-2807-45d8-b13e-11747c285dbd-c000.snappy.parquet', engine='pyarrow')
-print(df)
--- clear-mount-data.ps1
+++
@@ -1,3 +0,0 @@
-Remove-Item -Recurse -Force .\hadoop-namenode\name\*
-Remove-Item -Recurse -Force .\hadoop-datanode\data\*
-Remove-Item -Recurse -Force .\elasticsearch\data\*
